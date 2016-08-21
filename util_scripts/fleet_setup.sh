586fe9bc16aac88541bc81eb25410888f6536191
ivan <ivan@ivans-MacBook-Pro.local>
2016-08-21 18:02:23 -0400

    remove specifict hard coded paths on the fleet_setup.sh script

--- util_scripts/fleet_setup.sh
+++ util_scripts/fleet_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm /Users/ivan/.fleetctl/known_hosts
+rm /Users/*/.fleetctl/known_hosts
 export FLEETCTL_TUNNEL=127.0.0.1:2222
 ssh-add ~/.vagrant.d/insecure_private_key
 fleetctl list-machines
