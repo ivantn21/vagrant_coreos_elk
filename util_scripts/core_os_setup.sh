2df6d22038b14ad4ac871457426e13e7523f3628
ivan <ivan@ivans-MacBook-Pro.local>
2016-08-21 17:51:05 -0400

    removed extra lines on core_os_setup.sh script

--- util_scripts/core_os_setup.sh
+++ util_scripts/core_os_setup.sh
@@ -1,4 +1,2 @@
-cd coreos-vagrant
-git pull https://github.com/coreos/coreos-vagrant/
 DISCOVERY_TOKEN=`curl -s https://discovery.etcd.io/new` && perl -p -e "s@#discovery: https://discovery.etcd.io/<token>@discovery: $DISCOVERY_TOKEN@g" user-data.sample > user-data
 export NUM_INSTANCES=1
