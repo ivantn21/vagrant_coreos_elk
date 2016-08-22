e45db52da21e74ec835fe4716db52c86e5d3acb1
ivan <ivan@ivans-MacBook-Pro.local>
2016-08-22 03:49:46 -0400

    Moved the core setup script to the vagrant root dir

--- util_scripts/core_os_setup.sh
+++
@@ -1,2 +0,0 @@
-DISCOVERY_TOKEN=`curl -s https://discovery.etcd.io/new` && perl -p -e "s@#discovery: https://discovery.etcd.io/<token>@discovery: $DISCOVERY_TOKEN@g" user-data.sample > user-data
-export NUM_INSTANCES=1
